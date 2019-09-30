c3541ff90ca2ded79710e373e780caeff15b65ac
JosueDurand <josue.durand8@gmail.com>
2019-09-30 16:52:28 -0500

    spinner in fragment

--- app/src/main/java/com/example/sliderfragmenttabs/fragment/FragmentRegistro.java
+++ app/src/main/java/com/example/sliderfragmenttabs/fragment/FragmentRegistro.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.sliderfragmenttabs.R;
 
@@ -20,12 +22,20 @@ public class FragmentRegistro extends Fragment {
         // Required empty public constructor
     }
 
-
+    String[] ItemsUnidadMedia = {"Seleccionar Unidad de Medida:", "Kilogramo", "Hora",
+            "Caja", "Jaba", "Atado", "Pote", "Saco", "Aves", "Sin Unidad",};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_registro, container, false);
+        //return inflater.inflate(R.layout.fragment_fragment_registro, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment_registro, container, false);
+        Spinner logAs = (Spinner) view.findViewById(R.id.nice_spinner);
+       ArrayAdapter dataAdapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_spinner_item, ItemsUnidadMedia);
+       // dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        logAs.setAdapter(dataAdapter);
+
+        return view;
     }
 
 
--- app/src/main/java/com/example/sliderfragmenttabs/fragment/FragmentTareo.java
+++ app/src/main/java/com/example/sliderfragmenttabs/fragment/FragmentTareo.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sliderfragmenttabs.R;
 
@@ -18,15 +24,37 @@ public class FragmentTareo extends Fragment {
     public FragmentTareo() {
         // Required empty public constructor
     }
+    public static FragmentTareo newInstance(){
+        FragmentTareo fragment = new FragmentTareo();
 
-
-
+        return fragment;
+    }
+    String[] ItemsUnidadMedia = {"Seleccionar Unidad de Medida:", "Kilogramo", "Hora",
+            "Caja", "Jaba", "Atado", "Pote", "Saco", "Aves", "Sin Unidad",};
+    Spinner spinner;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_tareo, container, false);
+//        Spinner  logAs = (Spinner) getActivity().findViewById(R.id.nice_spinner);
+//        ArrayAdapter dataAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, ItemsUnidadMedia);
+//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        logAs.setAdapter(dataAdapter);
+//        return inflater.inflate(R.layout.fragment_fragment_tareo, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment_tareo, container, false);
+
+
+        return view;
     }
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
 
 
 }
