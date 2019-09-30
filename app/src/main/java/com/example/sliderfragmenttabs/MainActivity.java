17c69e98bcfc58bddf07cd0bf9cac3129bccba86
JosueDurand96 <josue.durand8@gmail.com>
2019-09-30 09:58:24 -0500

    listo

--- app/src/main/java/com/example/sliderfragmenttabs/MainActivity.java
+++ app/src/main/java/com/example/sliderfragmenttabs/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.sliderfragmenttabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.example.sliderfragmenttabs.adapter.ViewPagerAdapter;
+import com.example.sliderfragmenttabs.fragment.FragmentDefinicion;
+import com.example.sliderfragmenttabs.fragment.FragmentEmpleados;
+import com.example.sliderfragmenttabs.fragment.FragmentOpciones;
+import com.google.android.material.tabs.TabLayout;
+
+
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPager);
+
+        addTabs(viewPager);
+        ((TabLayout) findViewById(R.id.tabLayout)).setupWithViewPager( viewPager );
+    }
+
+    private void addTabs(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new FragmentDefinicion(), "DEFINICIÓN");
+        adapter.addFrag(new FragmentOpciones(), "OPCIONES");
+        adapter.addFrag(new FragmentEmpleados(), "EMPLEADO");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/sliderfragmenttabs/fragment/FragmentEmpleados.java
@@ -0,0 +1,32 @@
+package com.example.sliderfragmenttabs.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sliderfragmenttabs.R;
+
+
+public class FragmentEmpleados extends Fragment {
+
+    public FragmentEmpleados() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_empleados, container, false);
+    }
+
+
+}
